c87787c270f483bff3129e9af842e0ada189d091
李君易 <lijunyi@lijunyideMacBook-Pro.local>
2019-03-16 18:00:57 +0800

    完成vue + ts + element-UI的搭建

--- web/static-file/demo/build/webpack.base.conf.js
+++ web/static-file/demo/build/webpack.base.conf.js
@@ -71,6 +71,17 @@ module.exports = {
         options: {
           appendTsSuffixTo: [/\.vue$/],
         }
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)(\?\S*)?$/,
+        loader: 'file-loader'
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?\S*)?$/,
+        loader: 'file-loader',
+        query: {
+          name: '[name].[ext]?[hash]'
+        }
       }
     ]
   },
--- web/static-file/demo/src/App.vue
+++ web/static-file/demo/src/App.vue
@@ -1,25 +1,56 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <template>
+      <el-carousel
+        :interval="4000"
+        type="card"
+        height="200px"
+      >
+        <el-carousel-item
+          v-for="item in 6"
+          :key="item"
+        >
+          <h3>{{ item }}</h3>
+        </el-carousel-item>
+      </el-carousel>
+    </template>
   </div>
 </template>
 
-<script lang="ts">
-import Vue from 'vue'
-
-export default Vue.extend({
-  name: 'App'
-})
+<script>
+export default {
+  name: "app",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App"
+    };
+  }
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.el-carousel__item h3 {
+  color: #475669;
+  font-size: 14px;
+  opacity: 0.75;
+  line-height: 200px;
+  margin: 0;
+}
+
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n + 1) {
+  background-color: #d3dce6;
 }
 </style>
